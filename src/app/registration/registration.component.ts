abf5e34af91885b818c6618cdc2722ad5f11496d
brian <briankathukya2000@gmail.com>
2020-07-11 05:12:26 +0300

    user registration component

---
+++ src/app/registration/registration.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-registration',
+  templateUrl: './registration.component.html',
+  styleUrls: ['./registration.component.css']
+})
+export class RegistrationComponent implements OnInit {
+
+  constructor(private http:HttpClient) { }
+
+  onSubmit(data){
+    this.http.post("http://127.0.0.1:5000/register",data)
+    .subscribe((res)=>{
+      console.warn("result",res);
+    })
+  }
+
+  ngOnInit(): void {
+  }
+
+}
